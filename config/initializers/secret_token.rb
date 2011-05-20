aa711abeb8786973964021ef6a3f88d2edd4f860
Clint Shryock <clint@ctshryock.com>
2011-05-20 08:27:55 -0500

    Initial re-import after moving directories around

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '0056b9d3ef1ebd6d7f756349f6e16e0405883d9486289c894916fe1e4863e2f7813ada29dbf4d204c11a901f26daa6d551fc003be3e24c94dd5a243c65a360a9'
