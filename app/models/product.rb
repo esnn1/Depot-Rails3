c929c4a290f0ff501b5f0fe9f20e994e90007099
Clint Shryock <clint@ctshryock.com>
2011-06-03 08:56:28 -0500

    Added store controller, basic html formatting
    Added default_scope to products model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
+  
+  # Default all queries that start with the Product
+  # to be ordered by title
+  default_scope :order => 'title'
+
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
