c9346ecaa44d4a7e243c3667fc59b42be814a53e
Clint Shryock <clint@ctshryock.com>
2011-06-06 08:49:30 -0500

    Add silly session based counter for Playtime

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,13 @@ class StoreController < ApplicationController
 
   def index
     @products = Product.all
+
+    #  Count number of times user hits store#index,
+    #  for "Playtime"
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] = session[:counter] + 1
   end
 
 end
