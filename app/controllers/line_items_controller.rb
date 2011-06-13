8ecc29a80cf5c232a372ddd5f818b9c3b8137b56
Clint Shryock <clint@ctshryock.com>
2011-06-13 16:03:26 -0500

    Pass current_item to the template, and highlight it after adding

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -47,8 +47,8 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to(store_url) }
+        format.js   { @current_item = @line_item }
         format.xml  { render :xml => @line_item, :status => :created, :location => @line_item }
-        format.js
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @line_item.errors, :status => :unprocessable_entity }
