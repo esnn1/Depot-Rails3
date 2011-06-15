6c076079312dd427a00662a657c46003e8690d1e
Clint Shryock <clint@ctshryock.com>
2011-06-15 08:38:59 -0500

    Playtime: Empty cart via ajax, use blind_up effect

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -84,6 +84,7 @@ class CartsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to(store_url) }
       format.xml  { head :ok }
+      format.js   
     end
   end
 end
