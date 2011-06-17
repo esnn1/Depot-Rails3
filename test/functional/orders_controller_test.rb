2c9ff2af161f9153134400dd70a735f96911021f
Clint Shryock <clint@ctshryock.com>
2011-06-17 09:00:35 -0500

    Update the 'get new' test to run with a line item

--- test/functional/orders_controller_test.rb
+++ test/functional/orders_controller_test.rb
@@ -12,6 +12,10 @@ class OrdersControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    LineItem.create(:cart => cart, :product => products(:ruby))
+
     get :new
     assert_response :success
   end
