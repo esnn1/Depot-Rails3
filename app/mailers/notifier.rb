568073c848ede234ef78a0c3b0033af186577c31
Clint Shryock <clint@ctshryock.com>
2011-06-30 08:46:21 -0500

    Add line_item template for email Update notifier to have order parameter

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -6,10 +6,9 @@ class Notifier < ActionMailer::Base
   #
   #   en.notifier.order_received.subject
   #
-  def order_received
-    @greeting = "Hi"
-
-    mail :to => "to@example.org"
+  def order_received(order)
+    @order = order
+    mail :to => order.email, :subject => 'Pragmatic Store Order Confirmation'
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
