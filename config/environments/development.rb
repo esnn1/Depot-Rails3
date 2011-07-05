2b21ebd0165653b62bf105044fe8d49cb8025f9f
Clint Shryock <clint@ctshryock.com>
2011-07-05 08:15:42 -0500

    Update with html email template

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,7 @@ Depot::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+
   # Don't care to actually send mail
   config.action_mailer.delivery_method = :test
 
