16e08fc1e846c83919e5615cc72248884a9f7167
Clint Shryock <clint@ctshryock.com>
2011-07-07 08:24:07 -0500

    Update tests

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -2,6 +2,12 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   setup do
+    @input_attribtes = {
+      :name                  => "holly",
+      :password              => "linux",
+      :password_confirmation => "linux"
+    }
+
     @user = users(:one)
   end
 
@@ -18,10 +24,10 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, :user => @user.attributes
+      post :create, :user => @input_attribtes
     end
 
-    assert_redirected_to user_path(assigns(:user))
+    assert_redirected_to users_path
   end
 
   test "should show user" do
@@ -35,8 +41,8 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should update user" do
-    put :update, :id => @user.to_param, :user => @user.attributes
-    assert_redirected_to user_path(assigns(:user))
+    put :update, :id => @user.to_param, :user => @input_attribtes
+    assert_redirected_to users_path
   end
 
   test "should destroy user" do
