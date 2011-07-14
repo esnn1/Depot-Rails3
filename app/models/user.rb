c5aeade5d5548f295449dd316e2eed443005939e
Clint Shryock <clint@ctshryock.com>
2011-07-14 09:15:24 -0500

    Move require statement outside the class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
-class User < ActiveRecord::Base
+require 'digest/sha2'
 
-  require 'digest/sha2'
+class User < ActiveRecord::Base
 
   validates :name, :presence => true, :uniqueness => true
   validates :password, :confirmation => true
