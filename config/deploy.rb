1654bc189b8ea7e53c45c23ebaf6cee78e9b2fc5
Clint Shryock <clint@ctshryock.com>
2011-07-19 14:58:51 -0500

    Finish up deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,15 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "Rails3Depot"
+set :repository,  "clint@ctshryock.com:webapps/git/repos/rails3.git"
+set :deploy_to, "/home/clint/webapps/depot/depot_app"
 
 set :deploy_via, :remote_cache
 
-set :scm, :git
+set :scm, 'git'
+set :branch, 'master'
+set :use_sudo, false
+set :keep_releases, 4
+server 'depot.catsby.net', :app, :web, :db, :primary => true
+
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 role :web, "your web-server here"                          # Your HTTP server, Apache/etc
@@ -15,10 +21,27 @@ role :db,  "your slave db-server here"
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+  desc "reload the database with seed data"
+  task :seed do
+    rn "cd #{current_path}; rake db:seed RAILS_ENV=production"
+  end
+end
+
+
+
+# cleanup
+after "deploy:update", "deploy:cleanup"
+
+# update gems
+
+after "deploy:update_code", :bundle_install
+desc "install the necessary prerequisites"
+task :bundle_install, :roles => :app do
+  run "cd #{release_path} && bundle install"
+end
