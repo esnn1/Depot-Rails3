386ce6aa037b3306a08c0610abb31c026b597ad2
Clint Shryock <clint@ctshryock.com>
2011-08-09 08:23:22 -0500

    Fix ajax test now that I'm using jQuery.

--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -29,7 +29,7 @@ class LineItemsControllerTest < ActionController::TestCase
       xhr :post, :create, :product_id => products(:ruby).id
     end
     assert_response :success
-    assert_select_rjs :replace_html, 'cart' do
+    assert_select_jquery :html, '#cart' do
       assert_select 'tr#current_item td', /Programming Ruby 1.9/
     end
   end
