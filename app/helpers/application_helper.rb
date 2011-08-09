09545f4871cdfd71d69c2ef1b8aac496bed7e6b8
Clint Shryock <clint@ctshryock.com>
2011-08-09 08:17:59 -0500

    Move `total_price` to application_helper; I want it for both orders and the cart

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,9 @@ module ApplicationHelper
     end
     content_tag("div", attributes, &block)
   end
+
+  def total_cost(line_items)
+    price = line_items.to_a.sum {|item| item.total_price }
+    number_to_currency(price)
+  end
 end
